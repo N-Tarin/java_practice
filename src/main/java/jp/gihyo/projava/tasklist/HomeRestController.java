f8f008c541e29fa1438646a8576f9ee4d7641094
N-Tarin <karinuxxx1029@gmail.com>
2022-07-28 13:51:20 +0900

    モデル作製・タスクを追加するエンドポイント作製

--- src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
@@ -1,12 +1,30 @@
 package jp.gihyo.projava.tasklist;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.UUID;
+import java.util.List;
 
 @RestController
 public class HomeRestController {
-    @RequestMapping(value="resthello")
+
+    record TaskItem(String id, String task, String deadline, boolean done) {}
+    private List<TaskItem> taskItems = new ArrayList<>();
+
+    @GetMapping("/restadd")
+    String addItem(@RequestParam("task") String task,
+                   @RequestParam("deadline") String deadline) {
+        String id = UUID.randomUUID().toString().substring(0, 8);
+        TaskItem item = new TaskItem(id, task, deadline, false);
+        taskItems.add(item);
+
+        return "タスクを追加しました。";
+    }
+    @RequestMapping("/resthello")
     String hello() {
         return """
                 Hello.
