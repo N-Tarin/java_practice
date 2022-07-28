aae2ebf0b577ae4d2818ef7dc88980a268906f40
N-Tarin <karinuxxx1029@gmail.com>
2022-07-28 15:48:45 +0900

    タスクを一覧表示するエンドポイントの作成

--- src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
@@ -6,14 +6,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.UUID;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 public class HomeRestController {
 
     record TaskItem(String id, String task, String deadline, boolean done) {}
     private List<TaskItem> taskItems = new ArrayList<>();
+
+    @GetMapping("/restlist")
+    String listItems() {
+        String result = taskItems.stream()
+                .map(TaskItem::toString)
+                .collect(Collectors.joining(","));
+        return result;
+    }
 
     @GetMapping("/restadd")
     String addItem(@RequestParam("task") String task,
