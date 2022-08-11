0d5cfe656973b0a0e19ae2cfbe89b35a8cafb384
N-Tarin <karinuxxx1029@gmail.com>
2022-08-11 22:06:53 +0900

    CSS追加・登録ボタン機能追加

--- src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -2,18 +2,33 @@ package jp.gihyo.projava.tasklist;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/greeting")
-    String hello(Model model) {
-        LocalTime time = LocalTime.now();
-        String greeting = "Morning";
+    record TaskItem(String id, String task, String deadline, boolean done) {}
+    private List<TaskItem> taskItems = new ArrayList<>();
 
-        model.addAttribute("greeting", greeting);
-        model.addAttribute("time", time);
-        return "greeting";
+    @GetMapping("/list")
+    String listItems(Model model) {
+        model.addAttribute("taskList", taskItems);
+        return "home";
+    }
+
+    @GetMapping("/add")
+    String addItem(@RequestParam("task") String task,
+                   @RequestParam("deadline") String deadline) {
+        String id = UUID.randomUUID().toString().substring(0, 8);
+        TaskItem item = new TaskItem(id, task, deadline, false);
+        taskItems.add(item);
+
+        return "redirect:/list";
     }
 }
